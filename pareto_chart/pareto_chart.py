4e3958ff0cd345dc41d091e68924a220c1fcd770
Kiryl <musomania@protonmail.com>
2021-07-24 14:13:35 +0300

    upd

--- pareto_chart/pareto_chart.py
+++ pareto_chart/pareto_chart.py
@@ -7,8 +7,8 @@ def pareto_chart(collection):
     counts = (collection.value_counts().to_frame('counts')
               .join(collection.value_counts(normalize=True).cumsum().to_frame('ratio')))
 
-    fig = go.Figure([go.Bar(x=counts.index, y=counts['counts'], yaxis='y1', name=''),
-                     go.Scatter(x=counts.index, y=counts['ratio'], yaxis='y2', name='',
+    fig = go.Figure([go.Bar(x=counts.index, y=counts['counts'], yaxis='y1', name='count'),
+                     go.Scatter(x=counts.index, y=counts['ratio'], yaxis='y2', name='cumulative ratio',
                                 hovertemplate='%{y:.1%}', marker={'color': '#000000'})])
 
     fig.update_layout(template='plotly_white', showlegend=False, hovermode='x', bargap=.3,
@@ -16,7 +16,7 @@ def pareto_chart(collection):
                       yaxis={'title': 'count'},
                       yaxis2={'rangemode': "tozero", 'overlaying': 'y',
                               'position': 1, 'side': 'right',
-                              'title': 'cumulative ratio',
+                              'title': 'ratio',
                               'tickvals': np.arange(0, 1.1, .2),
                               'tickmode': 'array',
                               'ticktext': [str(i) + '%' for i in range(0, 101, 20)]})
