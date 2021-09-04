b44e5a9dc3da6d183a590c0964fcb31958bf80d4
Kiryl <musomania@protonmail.com>
2021-09-04 16:57:02 +0300

    yaxis2 range fix

--- unusual_charts/pareto_chart/pareto_chart.py
+++ unusual_charts/pareto_chart/pareto_chart.py
@@ -2,23 +2,21 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-def pareto_chart(collection):
+def pareto_chart(collection, title=None):
     collection = pd.Series(collection)
     counts = (collection.value_counts().to_frame('counts')
               .join(collection.value_counts(normalize=True).cumsum().to_frame('ratio')))
 
     fig = go.Figure([go.Bar(x=counts.index, y=counts['counts'], yaxis='y1', name='count'),
-                     go.Scatter(x=counts.index, y=counts['ratio'], yaxis='y2', name='cumulative ratio',
-                                hovertemplate='%{y:.1%}', marker={'color': '#000000'})])
+                     go.Scatter(x=counts.index, y=counts['ratio']*100, yaxis='y2', name='cumulative ratio',
+                                hovertemplate='%{y:.1f}%', marker={'color': '#000000'})])
 
     fig.update_layout(template='plotly_white', showlegend=False, hovermode='x', bargap=.3,
-                      title={'text': 'Pareto Chart', 'x': .5}, 
+                      title={'text': title, 'x': .5}, 
                       yaxis={'title': 'count'},
                       yaxis2={'rangemode': "tozero", 'overlaying': 'y',
                               'position': 1, 'side': 'right',
-                              'title': 'ratio',
-                              'tickvals': np.arange(0, 1.1, .2),
-                              'tickmode': 'array',
-                              'ticktext': [str(i) + '%' for i in range(0, 101, 20)]})
+                              'title': 'percantage (%)',
+                              'range':[0,110]})
 
     fig.show()
