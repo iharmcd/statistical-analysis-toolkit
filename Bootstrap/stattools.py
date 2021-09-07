3127bfd248f1d1f58880164faf91d8f5aacebabc
Kiryl <musomania@protonmail.com>
2021-09-07 17:04:10 +0300

    Revert "corr_ratio_anova_namedtuple"
    This reverts commit e5c3a4c280c2426f817b1fecf572529e9e9c8478.

--- Bootstrap/stattools.py
+++ Bootstrap/stattools.py
@@ -80,23 +80,16 @@ def correlation_ratio(categories, values):
        ssb: sum of squares between groups'''
     
     cat = np.unique(categories, return_inverse=True)[1]
-    uniq_cat = np.unique(cat)
     values = np.array(values)
-    deg_w = values.shape[0] - len(uniq_cat)
-    deg_b = len(uniq_cat) - 1
     
     ssw = 0
     ssb = 0
-    for i in uniq_cat:
+    for i in np.unique(cat):
         subgroup = values[np.argwhere(cat == i).flatten()]
         ssw += np.sum((subgroup-np.mean(subgroup))**2)
         ssb += len(subgroup)*(np.mean(subgroup)-np.mean(values))**2
-    
-    eta = (ssb / (ssb + ssw))**.5
-    F = (ssb/deg_b)/(ssw/deg_w)
-    pvalue = 1 - st.f.cdf(F, deg_b, deg_w)
-    stat = namedtuple('PearsonEtaResult',('corr','pvalue','statistic'))    
-    return stat(eta,pvalue,F)
+
+    return (ssb / (ssb + ssw))**.5
 
 
 def cramers_v(rc_table):
