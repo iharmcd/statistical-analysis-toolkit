f4ccc7a2194bb8f9d726911b725709536e2b4dfc
Kiryl <musomania@protonmail.com>
2021-09-09 17:06:36 +0300

    fixed_colorscale

--- unusual_charts/mosaic_chart/mosaic.py
+++ unusual_charts/mosaic_chart/mosaic.py
@@ -14,16 +14,16 @@ def mosaic_chart(rc_table, title=None, residuals=False):
     chart = []
     for i in heights.index:
         if residuals == 'standardized':
-            marker = dict(cmin=-4, cmax=4, color=standartized_residuals.loc[i], colorbar={'title': ''},
-                          colorscale='RdBu')
+            marker = dict(cmin=-4, cmax=4, color=standartized_residuals.loc[i], colorscale='RdBu',
+                          colorbar={'title': ''})
             customdata = [i] * len(labels)
             texttemplate = "%{customdata}: %{text:,}"
             showlegend = False
             
         elif residuals == 'percentage':
-            cmax = int(percentage_error.max().max() // 10 + 1) * 10 
-            marker = dict(cmin=-cmax, cmax=cmax, color=percentage_error.loc[i], colorbar={'title': ''},
-                          colorscale='RdBu')
+            marker = dict(cmin=-100, cmax=100, color=percentage_error.loc[i], colorscale='edge_r',
+                          colorbar={'ticktext':list(range(-100,101,20)), 
+                                    'tickvals':list(range(-100,101,20)), 'title': ''})
             customdata = [i] * len(labels)
             texttemplate = "%{customdata}: %{text:,}"
             showlegend = False
@@ -47,7 +47,7 @@ def mosaic_chart(rc_table, title=None, residuals=False):
     fig.update_layout(template='simple_white', barmode="stack", uniformtext={'mode': "hide", 'minsize': 12},
                       yaxis={'range': [0, 100], 'title':'percentage (%)'},
                       xaxis={'range': [0, 100], 'tickvals': np.cumsum(widths) - widths / 2,
-                      'ticktext': ["{}<br>{:,}".format(l, w) for l, w in zip(labels, rc_table.sum(axis=0).tolist())]},
+                'ticktext': ["{}<br>{:,}".format(l, w) for l, w in zip(labels, rc_table.sum(axis=0).tolist())]},
                       title={'text': title, 'x': .5}, showlegend=showlegend,
                       legend_title_text=rc_table.index.name)
     fig.show()
