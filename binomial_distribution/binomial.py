5b45cdcd9c11faa6caef55ab8c3bc3a6d2f4d113
Kiryl S <carollgplay@gmail.com>
2022-11-06 14:58:51 +0300

    added cdf axis

--- binomial_distribution/binomial.py
+++ binomial_distribution/binomial.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import scipy.stats as st
 import plotly.graph_objects as go
@@ -9,16 +6,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, compress=False)
-
+#server = app.server
 app.layout = html.Div(children=[
 
-		html.Center(html.H2(children = 'Responce Estimation with Binomial Distribution')),
+		html.Center(html.H2(children = 'Binomial Distribution')),
 		html.Hr(),
-		html.Div(["Input number of messages: ", dcc.Input(id='trials', type='number', step=1, max=1001, min=0, value=10)]),
+		html.Div(["Number of trials: ", dcc.Input(id='trials', type='number', step=1, max=1001, min=0, value=10)]),
 		html.Br(),
-		html.Div(["Expected conversion rate: ", dcc.Input(id='probas',type='number', step=0.01, min=0, max=1, value=0.5)]),
+	html.Div(["Probability: ", dcc.Input(id='probas',type='number', step=0.001, min=0, max=1, value=0.5)]),
 		html.Hr(),
 		html.Div([dcc.Graph( id = 'binomial_chart')], className='twelve columns'),
 		html.H6(html.Div(id='output-info', style={'width': '95%','float':'right'})),
@@ -36,15 +34,29 @@ def update_figures(trials, prob):
 	def binominal(n, p):
 		n_range = np.array(range(n+1))
 		return n_range, st.binom.pmf(n_range, n, p)
-
+    
+	def cumsum(probas):
+       		 to_sum = 0
+       		 cumsum = [to_sum := to_sum + i for i in probas]
+       		 return cumsum
+    
 	bar_chart = []	
 	description = ''
 
 	try:
 		trials_range, binom_pmf = binominal(trials,prob)
-		bar_chart = [(go.Bar(x=trials_range,y=binom_pmf, hoverinfo='text+x', text=list(map(lambda x: '{:.1%}'.format(x),binom_pmf)), marker_color='#FFA15A',
-			opacity=0.85))]
-
+		binom_chart = go.Figure([go.Bar(x=trials_range,y=binom_pmf, 
+                                        hoverinfo='text+x+name',yaxis='y1', 
+                                        text=list(map(lambda x: '{:.1%}'.format(x),binom_pmf)), textposition='inside',
+                                        marker_color='#FFA15A', opacity=0.85, name='cdf'),
+                                 go.Scatter(x=trials_range, y=cumsum(binom_pmf), name='pmf', marker_color='#636EFA', opacity=0.85,
+                                 mode='lines+markers',yaxis='y2')])
+		binom_chart.update_layout(template='plotly_white', hovermode='x', xaxis={'title':'possible outcomes'}, 
+                                yaxis={'title':'proba pmf','tickformat':'.1%'},
+				yaxis2={'overlaying': 'y', 'position': 1, 'side': 'right','rangemode':'tozero',
+					'tickformat':'.1%','title':'proba cdf'},
+				uniformtext_minsize=10, uniformtext_mode='hide')
+        
 		mx, var, _, _ = st.binom.stats(trials, prob, moments='mvsk')
 	
 		description = 'Mean ± Std: {:.0f} ± {:.3f}'.format(mx, var**.5)
@@ -52,16 +64,9 @@ def update_figures(trials, prob):
 		pass
 
 	return (
-		{'data':  bar_chart, 
-		'layout': go.Layout(template='plotly_white', hovermode='x', xaxis={'title':'possible outcomes'}, yaxis={'title':'probability'})},
+       		 binom_chart,
 		description,
 	)
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0')
-
-
-
-
-
-
