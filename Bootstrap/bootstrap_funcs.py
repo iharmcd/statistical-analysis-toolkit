1fed73502d6df53b87c2260841ec420d0666ea79
Kiryl S <carollgplay@gmail.com>
2023-07-08 12:44:58 +0300

    Update bootstrap_funcs.py

--- Bootstrap/bootstrap_funcs.py
+++ Bootstrap/bootstrap_funcs.py
@@ -15,38 +15,7 @@ def bootstrap_ab(a,b, stat=np.mean, confidence_level=0.95, boot_size=10_000, two
     #uplift_ci = np.quantile((statistic_b - statistic_a) / statistic_a, q=[(1-confidence_level)/2,1-(1-confidence_level)/2])
     return min(p*2, 2-p*2) if two_tailed else p, tuple(diff_ci), #tuple(uplift_ci)
       
-      
-def monte_carlo_area(x, y, num_samples=1_000_000, random_state=None):
-    np.random.seed(random_state)
-    min_x,max_x = min(x), max(x)
-    min_y,max_y = min(y),max(y)
-    # Сгенерировать случайные точки внутри этой области
-    rand_x = np.random.uniform(min_x, max_x, size=num_samples)
-    rand_y = np.random.uniform(min_y, max_y, size=num_samples)
-    # Проверить, попадает ли точка внутрь кривой.
-    # Это можно сделать, например, с помощью ломаных.
-    # Для этого нужно соединить точки (x, y) ломаной и проверить,
-    # пересекает ли ломаная точку (rand_x, rand_y).
-    # Если точка пересекается с ломаной нечетное число раз,
-    # значит она находится внутри кривой.
-    num_under_curve = 0
-    for i in range(len(x) - 1):
-        x1, y1 = x[i], y[i]
-        x2, y2 = x[i + 1], y[i + 1]
-        condition = (rand_x >= min(x1, x2)) == (rand_x <= max(x1, x2)) 
-        r_x = rand_x[condition]
-        r_y = rand_y[condition]
-        # Проверить, пересекает ли точка ломаную
-        # (x1, y1) - (x2, y2) на заданном интервале
-        y_intersect = y1 + (r_x - x1) * (y2 - y1) / (x2 - x1)
-        num_under_curve += (y_intersect >= r_y).sum()
-    # Вычислить площадь под кривой как долю точек,
-    # которые попали под кривую, умноженную на площадь исходной области.
-    ratio = (num_under_curve / num_samples)
-    area =  ratio * ((max_x - min_x) * (max_y - min_y))
-    return ratio, area
-  
-  
+          
 def fwer(n_comparison:int, alpha=0.05):
     '''multiple comaprison error'''
     return 1-(1-alpha)**n_comparison
